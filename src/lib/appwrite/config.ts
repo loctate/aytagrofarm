6d7a5f0511cbc01058db53c93e3757cf2d0045e9
loctate <41179958+loctate@users.noreply.github.com>
2026-07-10 17:07:41 +0000

    feat: add ayt activities and photo storage service

--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -12,6 +12,10 @@ export const appwriteConfig = {
     process.env.NEXT_PUBLIC_APPWRITE_HPDKI_ACTIVITIES_TABLE_ID ?? "",
   hpdkiActivityPhotosBucketId:
     process.env.NEXT_PUBLIC_APPWRITE_HPDKI_ACTIVITY_PHOTOS_BUCKET_ID ?? "",
+  aytActivitiesTableId:
+    process.env.NEXT_PUBLIC_APPWRITE_AYT_ACTIVITIES_TABLE_ID ?? "",
+  aytActivityPhotosBucketId:
+    process.env.NEXT_PUBLIC_APPWRITE_AYT_ACTIVITY_PHOTOS_BUCKET_ID ?? "",
   knowledgeTableId:
     process.env.NEXT_PUBLIC_APPWRITE_KNOWLEDGE_TABLE_ID ?? "",
   ktaSettingsTableId:
